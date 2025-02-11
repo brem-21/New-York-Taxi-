75a1833e9408d1b490140efc74f311ac381bec08
Brempong Appiah Dankwah <brempong.dankwah@amalitech.com>
2025-02-11 04:36:31 -0500

    refactoring ingest_data.py and adding more requirements

--- ingest_data.py
+++ ingest_data.py
@@ -2,23 +2,23 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-#link to data on the ny_taxi's website
-url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz'
+# link to data on the ny_taxi's website
+url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz"
 
 # Loading data from source
 try:
-    data = pd.read_csv(url) #reading the data
+    data = pd.read_csv(url)  # reading the data
 except Exception as e:
-    print(f"Error Loading data from source: {e}") # Error handling
+    print(f"Error Loading data from source: {e}")  # Error handling
 else:
-    print("Data Loaded successfully") 
-    print(data.head(5)) # first five rows of the data
+    print("Data Loaded successfully")
+    print(data.head(5))  # first five rows of the data
 
-#Converting the data type text of pickup date and dropoff date to Timestamp
-data['lpep_pickup_datetime'] = pd.to_datetime(data['lpep_pickup_datetime'])
-data['lpep_dropoff_datetime'] = pd.to_datetime(data['lpep_dropoff_datetime'])
+# Converting the data type text of pickup date and dropoff date to Timestamp
+data["lpep_pickup_datetime"] = pd.to_datetime(data["lpep_pickup_datetime"])
+data["lpep_dropoff_datetime"] = pd.to_datetime(data["lpep_dropoff_datetime"])
 
-dataset_with_zones = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv'
+dataset_with_zones = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
 
 try:
     # Load the dataset
@@ -32,16 +32,18 @@ except pd.errors.EmptyDataError:
 
 
 try:
-    engine = create_engine('postgresql://root:root@localhost:5435/ny_taxi') #create the connection
-    engine.connect # connect to the database
+    engine = create_engine(
+        "postgresql://root:root@localhost:5435/ny_taxi"
+    )  # create the connection
+    engine.connect  # connect to the database
 except Exception as e:
     print(f"Error while connecting to database: {e}")
 else:
     print("Connection Sucessful")
 
-# sending the schema to the database 
+# sending the schema to the database
 try:
-    data.head(n=0).to_sql(name='green_taxi_data',con=engine, if_exists='replace')
+    data.head(n=0).to_sql(name="green_taxi_data", con=engine, if_exists="replace")
 except Exception as e:
     print(f"Failed to load schema into the database:, {e}")
 else:
@@ -49,7 +51,7 @@ else:
 
 # loading the data
 try:
-    %time data.to_sql(name='green_taxi_data',con=engine, if_exists='replace')
+    data.to_sql(name="green_taxi_data", con=engine, if_exists="replace")
 except Exception as e:
     print(f"Failed to load data into the database:, {e}")
 else:
@@ -57,15 +59,15 @@ else:
 
 # Loading the schema of the dataset with zones
 try:
-    %time dataset_with_zones.head(n=0).to_sql(con=engine, name='zones', if_exists='replace')
+    dataset_with_zones.head(n=0).to_sql(con=engine, name="zones", if_exists="replace")
 except Exception as e:
     print(f"Failed to load schema into the database:, {e}")
 else:
     print("schema loaded successfully into the database")
 
-#Loading the dataset_with_zones to the database
+# Loading the dataset_with_zones to the database
 try:
-    %time dataset_with_zones.to_sql(con=engine, name='zones', if_exists='replace')
+    dataset_with_zones.to_sql(con=engine, name="zones", if_exists="replace")
 except Exception as e:
     print(f"Failed to load data into the database:, {e}")
 else:
